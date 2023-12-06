c0a6c6719dc0da89e0858cd31ac6efa240b0b802
Temirlan-Br <barakov.temirlan@gmail.com>
2023-12-06 20:15:16 +0600

    Update background color, add Routers inApp.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
