be5198f0aba00998bfe55d5990673f43eeec4457
Temirlan-Br <barakov.temirlan@gmail.com>
2023-12-06 20:13:37 +0600

    Add Login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LogoBlock from '../../components/Logo/LogoBlock';
+import './Login.css';
+
+const Login = () => {
+  return (
+    <div>
+      <LogoBlock />
+    </div>
+  );
+};
+
+export default Login;
