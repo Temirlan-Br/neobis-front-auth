6ce08f93546be450ecde3d19b4fa6c414b063784
Temirlan-Br <barakov.temirlan@gmail.com>
2023-12-07 21:12:25 +0600

    Add Registration Block

---
+++ src/components/Registration/RegBlock.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './RegBlock.css';
+
+const RegBlock = () => {
+  return (
+    <div>
+      <div className="form__block">
+        <h3 className="form__title">Создать аккаунт</h3>
+        <form className="form">
+          <input
+            type="email"
+            placeholder="Введи адрес почты"
+            className="form__input"
+          />
+
+          <input
+            type="text"
+            placeholder="Введи туда-сюда логин"
+            className="form__input"
+          />
+          <div className="password__block">
+            <input
+              type="password"
+              placeholder="Создай пароль"
+              className="form__input"
+            />
+          </div>
+          <div className="password__block">
+            <input
+              type="password"
+              placeholder="Повтори пароль"
+              className="form__input"
+            />
+          </div>
+
+          <button className="btn-to-log">Войти</button>
+        </form>
+
+        <div className="link-to-log">
+          <Link to={'/'} className="link-to-log">
+            У меня есть аккаунт
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegBlock;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LogoBlock from '../../components/Logo/LogoBlock';
 import './Login.css';
+import RegBlock from '../../components/Registration/RegBlock';
 
 const Login = () => {
   return (
-    <div>
+    <div className="container">
       <LogoBlock />
+      <RegBlock />
     </div>
   );
 };
