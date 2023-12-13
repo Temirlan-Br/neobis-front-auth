a5ea71bec333e5611767cb79917a88edb6aa4f67
Temirlan-Br <barakov.temirlan@gmail.com>
2023-12-14 00:19:47 +0600

    Add email page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/Main/MainPage';
 import RegPage from './pages/Registr/RegPage';
+import Email from './pages/Email/Email';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/reg" element={<RegPage />} />
+          <Route path="/mail" element={<Email />} />
         </Routes>
       </BrowserRouter>
     </div>
